ccf8653180636248af3d96b66db1282a705d3663
Tom Vladeck <thomas.vladeck@gmail.com>
2013-10-06 14:20:56 -0700

    added some ish

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,12 @@
+class DashboardController < ApplicationController
+
+  def new
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+
+end
--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,2 +1,22 @@
 class Dashboard < ActiveRecord::Base
+
+  class << self
+    # methods to pull all the Dashboard entries w/in given periods of time
+    def yearly
+      # todo
+    end
+    
+    def monthly
+      # todo
+    end
+    
+    def weekly
+      # todo
+    end
+  end
+
+private
+  
+
+
 end
